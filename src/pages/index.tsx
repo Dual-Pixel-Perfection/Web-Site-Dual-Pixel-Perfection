79958d988eae5446ed84b3a35694afa7d54148b7
Adryelli <adryellireis0santos@gmail.com>
2021-08-30 22:19:35 -0300

    add colors theme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,6 +6,9 @@ import styles from '../styles/Home.module.css'
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Dual Pixel Perfection Studio</title>
+      </Head>
       
     </div>
   )
